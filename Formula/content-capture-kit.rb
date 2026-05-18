16e464fec6f7abcabb683b86bfc190da67934c0b
焰焰 say <feikool@sina.com>
2026-05-18 21:11:52 +0800

    Update Homebrew formula for v0.1.7

--- Formula/content-capture-kit.rb
+++ Formula/content-capture-kit.rb
@@ -4,8 +4,8 @@ class ContentCaptureKit < Formula
   desc "Get X, WeChat, and web articles as Markdown for knowledge bases"
   homepage "https://github.com/yanyansay/content-capture-kit"
   url "https://github.com/yanyansay/content-capture-kit.git",
-      tag:      "v0.1.6",
-      revision: "4535ecea907948c12dddc88479e38ce3d1df019b"
+      tag:      "v0.1.7",
+      revision: "b065aa3f3031093e96954cf3f169564b818986a0"
   head "https://github.com/yanyansay/content-capture-kit.git", branch: "main"
 
   depends_on "python@3.12"
